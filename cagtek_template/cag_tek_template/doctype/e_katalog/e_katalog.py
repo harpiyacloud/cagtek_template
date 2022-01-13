01bf10656eacdf0f29ca788433f14aba1abc3b73
Yasir Akel <yasir@harpiya.com>
2022-01-13 11:03:22 +0300

    Same change

--- cagtek_template/cag_tek_template/doctype/e_katalog/e_katalog.py
+++ cagtek_template/cag_tek_template/doctype/e_katalog/e_katalog.py
@@ -13,6 +13,13 @@ class EKatalog(WebsiteGenerator):
         if not self.route:
             self.route = 'e-catalog/' + self.scrub(self.title)
 
+    def on_update(self):
+        super(EKatalog, self).on_update()
+        clear_cache()
+
+    def on_trash(self):
+        super(EKatalog, self).on_trash()
+
 
 def get_list_context(context):
     context.allow_guest = True
@@ -20,3 +27,8 @@ def get_list_context(context):
     context.show_sidebar = False
     context.title = _("E-Katalog")
     context.no_breadcrumbs = False
+
+
+def clear_cache():
+    from harpiya.website.utils import clear_cache
+    clear_cache()
